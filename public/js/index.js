5e83762a67dbf7009b6143f3e7149dc66dd11c01
Vincent Brouwer <kipsensatie@gmail.com>
2017-06-12 12:32:01 +0200

    I dont even know

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,16 @@
+var currpage = 0, pages = [];
 $(document).ready(function() {
-  loadData();
+  pages[0] = 'return Math.floor((Math.random() * 99) + 0)';
+  pages[1] = 'return crypto.getRandomValues()';
+  loadData(new Function(pages[currpage]));
   $('#refresh').click(function() {
-    loadData();
+    loadData(new Function(pages[currpage]));
   });
 });
 
 
-function loadData() {
-  var data = refreshDataSets();
+function loadData(func) {
+  var data = refreshDataSets(func);
   var chart = new CanvasJS.Chart("results", {
     title:{
       text: "Resultaten"
@@ -56,14 +59,28 @@ function loadData() {
   chart.render();
 }
 
-function refreshDataSets() {
+function pageForward() {
+  if(currpage < 1) {
+    currpage++;
+    $("#alg").text(pages[currpage]);
+  }
+}
+
+function pageBack() {
+  if(currpage > 0) {
+    currpage--;
+    $("#alg").text(pages[currpage]);
+  }
+}
+
+function refreshDataSets(func) {
   var alldata = [];
   alldata[0] = [];
   alldata[1] = [];
   alldata[2] = [];
   var data = alldata[0];
   for(i=0;i<100000;i++) {
-    var rnd = Math.floor((Math.random() * 99) + 0);
+    var rnd = func();
     if(data[rnd]) {
       if(data[rnd].y < 1099) {
         data[rnd].y = data[rnd].y+1;
@@ -74,7 +91,7 @@ function refreshDataSets() {
   }
   var data = alldata[1];
   for(i=0;i<50000;i++) {
-    var rnd = Math.floor((Math.random() * 99) + 0);
+    var rnd = func();
     if(data[rnd]) {
       data[rnd].y = data[rnd].y+1;
     } else {
@@ -83,7 +100,7 @@ function refreshDataSets() {
   }
   var data = alldata[2];
   for(i=0;i<10000;i++) {
-    var rnd = Math.floor((Math.random() * 99) + 0);
+    var rnd = func();
     if(data[rnd]) {
       data[rnd].y = data[rnd].y+1;
     } else {
